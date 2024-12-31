38ba7db86134e78fb29125fdf9556bcbbd1a8eeb
Sayeed Afridi <as.afridi563@gmail.com>
2024-12-31 18:23:54 +0600

    feat: navigation bar at top

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,17 @@
 import type { Metadata } from 'next';
 import { Rubik } from 'next/font/google';
 import './globals.css';
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from '@src/components/ui/navigation-menu';
+import Image from 'next/image';
+import Link from 'next/link';
 
-const sofia = Rubik({
+const rubik = Rubik({
   variable: '--font-rubik',
 });
 
@@ -18,7 +27,37 @@ export default function RootLayout({
 }>) {
   return (
     <html className='dark' lang='en'>
-      <body className={`${sofia.variable} antialiased`}>{children}</body>
+      <body className={`${rubik.variable} antialiased`}>
+        <div className='flex flex-row justify-between p-8'>
+          <NavigationMenu className='flex items-center'>
+            <NavigationMenuList>
+              <Image src='/algolens.webp' alt='logo' width={32} height={32} />
+              <Link href='/'>
+                <NavigationMenuItem>Algolens</NavigationMenuItem>
+              </Link>
+            </NavigationMenuList>
+          </NavigationMenu>
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <Link legacyBehavior passHref href='/team'>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Team
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link legacyBehavior passHref href='/about'>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    About
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
+        {children}
+      </body>
     </html>
   );
 }
