2d07f3e572ffa2f4d1abbe1de8697c6ad0245a35
Sayeed Afridi <as.afridi563@gmail.com>
2025-01-03 15:20:39 +0600

    feat: linear search visualisation

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+export function generateRandomArray(length = 10, maxValue = 200) {
+  return Array.from({ length }, () => Math.floor(Math.random() * maxValue));
+}
+
+export function delay(time = 500) {
+  return new Promise((res) => setTimeout(res, time));
+}
