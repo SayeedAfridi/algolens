b4e23affa9a57a245221c01738632852a2437d8f
sabbir-veer <sabbir.hstu.19@gmail.com>
2025-01-30 22:12:02 +0600

    refactor(refactor folder and files of about): moved files from ui components to features

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,21 +1,14 @@
-import AboutAction from '@src/components/aboutComponents/AboutAction';
-import AboutFeature from '@src/components/aboutComponents/AboutFeature';
-import AboutHero from '@src/components/aboutComponents/AboutHero';
-import AboutQuery from '@src/components/aboutComponents/AboutQuery';
+import AboutAction from '@src/features/aboutComponents/AboutAction';
+import AboutFeature from '@src/features/aboutComponents/AboutFeature';
+import AboutHero from '@src/features/aboutComponents/AboutHero';
+import AboutQuery from '@src/features/aboutComponents/AboutQuery';
 
 export default function AboutPage() {
   return (
     <div className='min-h-screen bg-backgroundExtra text-gray-900'>
-      {/* Hero Section */}
       <AboutHero />
-
-      {/* Features Section */}
       <AboutFeature />
-
-      {/* How It Works */}
       <AboutQuery />
-
-      {/* Call to Action */}
       <AboutAction />
     </div>
   );
